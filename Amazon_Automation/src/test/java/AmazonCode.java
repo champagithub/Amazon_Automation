a00fbb6421116499054121ebca28c1023b54032e
nandi <nandi@DESKTOP-H2MOL6B.lan>
2022-07-11 19:31:06 -0400

    right or wrong

---
+++ Amazon_Automation/src/test/java/AmazonCode.java
@@ -0,0 +1,17 @@
+
+public class AmazonCode {
+	static String name="bangladesh";
+	static int a=50;
+	int b=60;
+	
+	public static void main(String[] args) {
+		System.out.println(name);
+		System.out.println(a);
+		AmazonCode objname=new AmazonCode();
+		System.out.println(objname.b);	
+
+	}
+public static void local() {
+int c=70;
+}
+}
---
+++ Amazon_Automation/src/test/java/BankCode.java
@@ -0,0 +1,12 @@
+
+public class BankCode {
+	public static void main(String[] args) {
+		int chasebank=20;
+		int HSBCBANK=30;
+		int TDBANK=40;
+		int total=chasebank+HSBCBANK+TDBANK;
+		System.out.println(total);
+		System.out.println("total amount of balance $"+total);
+	}
+
+}
